5cddae5e3ff47df0db688b8e0494f2bc0d734c99
0xJosh <the_oog@protonmail.com>
2018-04-17 21:03:37 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # malsearch
-
-![alt text](malsearch.png)
-
-The purpose of this project is to see if the binary is malicious or not. Malsearch generates a sha256 checksum of the given binary then uses VirusTotal's public API to see if any AV vendors have classed the sample as malicious or not.
+The purpose of this project is to see if the binary is malicious or not, running a hash check with VirusTotal using VT's public API.
--- malsearch.py
+++ malsearch.py
@@ -29,10 +29,10 @@ try:
     config = configparser.ConfigParser()
     config.read(args.i)
     VT_PUBLIC_API_KEY = config['DEFAULT']['VT_PUBLIC_API_KEY']
-
 except Exception as e:
     traceback.print_exc()
-    print("[E] Config file '%s' not found" % args.i)
+    print("[?] Config file '%s' not found" % args.i)
+
 
 # Virus Total Report URL
 VT_REPORT_URL = 'https://www.virustotal.com/vtapi/v2/file/report'
@@ -50,47 +50,53 @@ def getHash(fname):
 
 def get_vt_info(hash):
     file_info = {
-        'hash':         hash,
-        'file_name':     '-',
-        'other_names':    [],
-        'file_type':     '-',
-        'submit_date':   '-',
-        'md5':           '-',
+        'hash':         hash, # hash we have used for scanning
+        'file_name':     '-', # filename of scanned file
+        'other_names':    [], # Other filenames used in wild etc.
+        'file_type':     '-', # pdf or exe?
+        'submit_date':   '-', # date of VT submission
+        'md5':           '-', 
         'sha1':          '-',
         'sha256':        '-',
-        'vendors':         [],
-        'vendors_summary': [],
-        'permalink':      ''
+        'vendors':        {},
+        'permalink':      '',
+        'error':          False
 
     }
     
-    try:
-        params = {'apikey': VT_PUBLIC_API_KEY, 'resource': hash}
-        vt_response = requests.get(VT_REPORT_URL, params=params)
-        vt_status_code = vt_response.status_code
-        vt_response_json = vt_response.json()
-        if vt_status_code > 0:
-            file_info['md5']         = str(vt_response_json.get('md5'))
-            file_info['sha1']        = str(vt_response_json.get('sha1'))
-            file_info['sha256']      = str(vt_response_json.get('sha256'))
-            file_info['submit_date'] = str(vt_response_json.get('scan_date'))
-            file_info['permalink']   = str(vt_response_json.get('permalink'))
-            file_info['vendors']     = [str(vendor) for vendor in vt_response_json.get('scans')]
+    params = {'apikey': VT_PUBLIC_API_KEY, 'resource': hash}
+    vt_response = requests.get(VT_REPORT_URL, params=params)
+    vt_status_code = vt_response.status_code
+    vt_response_json = vt_response.json()
 
-            info = parsePermalink(file_info['permalink'])
-            file_info.update(info)
-            return file_info
-
-    except Exception as e:
-        traceback.print_exc()
-
+    # Check to see if hash has already been scanned.
+    if vt_response_json.get("verbose_msg").startswith('Invalid'):
+        file_info['error'] = True
+        os.close(0)
     
+    elif vt_status_code == 403:
+        return "Permission denied. Do you have an valid API key?."
+        
+    elif vt_status_code == 200:
+        file_info['md5']          = str(vt_response_json.get('md5'))
+        file_info['sha1']         = str(vt_response_json.get('sha1'))
+        file_info['sha256']       = str(vt_response_json.get('sha256'))
+        file_info['submit_date']  = str(vt_response_json.get('scan_date'))
+        file_info['permalink']    = str(vt_response_json.get('permalink'))
+        file_info['file_name']    = args.f
+        file_info['vendors']      = {str(vend_name):str(result.get('result')) for vend_name, result  in vt_response_json['scans'].items()}
+
+        # Parse permalink for more info.
+        info = parsePermalink(file_info['permalink'])
+        file_info.update(info)
+        return file_info
+
 def parsePermalink(permalink):
     additional_info = {
-        'file_name':     '',
         'other_names':   [],
         'file_type':     '',
-        'comments':      [], }
+        'comments':      [], 
+    }
     
     header = {'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.1b3) Gecko/20090305 Firefox/3.1b3 GTB5',
                'Referrer':   'https://www.virustotal.com/en/' }
@@ -106,9 +112,6 @@ def parsePermalink(permalink):
             if string.startswith('File type'):
                 additional_info['file_type'] = str(string[10:])
 
-        # Get file name
-        additional_info['file_name'] = args.f
-
         # Get other names
         elements = soup.find_all('td')
         for i, row in enumerate(elements):
@@ -122,12 +125,36 @@ def parsePermalink(permalink):
 
     finally:
         return additional_info
+    
+def parseResults():
 
-def processResults(info):
-    print 'File name: %s info['file_name']
+    fhash = getHash(args.f)
+    vt_result = get_vt_info(fhash)
 
+    print "[+] Getting hash..."
+    print "Hash:{}".format(fhash)
+    print "[+] Checking Virus Total..."
 
-test = getHash(args.f)
-url = get_vt_info(test)
-print get_vt_info(test)
+    if not vt_result['error']:
+        print "[+] Sample has been reported, displaying results..."
+        
+
+        print """
+        *--------------------------------------------------------------------------------
+        * Hashes
+        *--------------------------------------------------------------------------------
+        * MD5:    {0}
+        * SHA1:   {1}
+        * SHA256: {2}
+        *--------------------------------------------------------------------------------
+        *
+
+        """.format(vt_result['md5'], vt_result['sha1'], vt_result['sha256'])
+
+    else:
+        print "[-] Can't find anything on Virus Total; proceed with caution." 
+        os.close(0)
+    
+print parseResults()
+
 
